cc8fddf7ff51f1a8169864519588c436f8943f9a
Orca Project <shamukillaz@orcaproject.co>
2013-06-23 03:32:12 -0400

    Build Fix - This is Orca

--- core/product_config.mk
+++ core/product_config.mk
@@ -186,7 +186,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
   ifneq ($(BF_PRODUCT),)
-    all_product_configs := $(shell ls vendor/bigfoot/products/${BF_PRODUCT}.mk)
+    all_product_configs := $(shell ls vendor/orca/products/${BF_PRODUCT}.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
